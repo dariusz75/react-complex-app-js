67e4f7d2bf5ec59492c1342ce2c07f2a2911dccc
Dariusz Franczak <darius32@op.pl>
2020-09-16 20:56:16 +0100

    reusable page component created

---
+++ src/components/Container.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Container(props) {
+  return (
+    <div
+      className={`${
+        props.wide ? "container py-md-5" : "container container--narrow py-md-5"
+      }`}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Container;
